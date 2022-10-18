e17c16e74207ade345187525b3bf98d32acf55c4
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-10-18 23:27:35 +0300

    Перенесена информация

---
+++ Lvl_31/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.WriteLine("Введите количество элементов массива:");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[num];
+
+void FillArray(int[] col)
+{
+    int lenght = col.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        col[index] = new Random().Next(-9, 10);
+        index++;
+    }
+}
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+    Console.WriteLine("Сгенерированный массив:");
+    for (int i = 0; i < count; i++)
+    {
+        if (i == count - 1) Console.Write($"{arr[i]} ");
+        else Console.Write($"{arr[i]}, ");
+        
+    }
+    
+}
+Console.WriteLine();
+int SummUnder(int[] arr)
+{
+    int underZero = default;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] <= 0) underZero = underZero + arr[i];
+    }
+    return underZero;
+}
+
+int SummUp(int[] arr)
+{
+    int upZero = default;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) upZero = upZero + arr[i];
+    }
+    return upZero;
+}
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+int sumUnder = SummUnder(array);
+int sumUp = SummUp(array);
+
+Console.WriteLine($"Сумма отрицательных элементов массива = {sumUnder}");
+Console.WriteLine($"Сумма положительных элементов массива = {sumUp}");
---
+++ Lvl_32/Program.cs
@@ -0,0 +1,42 @@
+﻿
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[num];
+
+void FillArray(int[] col)
+{
+    int lenght = col.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        col[index] = new Random().Next(-9, 10);
+        index++;
+    }
+    Console.WriteLine();
+}
+void SwitchNum(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = arr[i] * -1;
+    }
+}
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+    Console.WriteLine("Сгенерированный массив:");
+    for (int i = 0; i < count; i++)
+    {
+        if (i == count - 1) Console.Write($"{arr[i]} ");
+        else Console.Write($"{arr[i]}, ");
+        
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+FillArray(array);
+Console.WriteLine("До:");
+PrintArray(array);
+SwitchNum(array);
+Console.WriteLine("После:");
+PrintArray(array);
