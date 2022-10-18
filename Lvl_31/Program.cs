92607cc0a0428cd1a3f26df1bc4f3f4bf339aee8
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-10-19 01:52:54 +0300

    Сделаны 33 и 32 задачи

--- Lvl_31/Program.cs
+++ Lvl_31/Program.cs
@@ -53,3 +53,56 @@ int sumUp = SummUp(array);
 
 Console.WriteLine($"Сумма отрицательных элементов массива = {sumUnder}");
 Console.WriteLine($"Сумма положительных элементов массива = {sumUp}");
+
+// Создание массива
+
+// int[] CreateArrayRndInt(int size, int min, int max)
+// {
+// int[] array = new int[size];
+// var rnd = new Random();
+
+// for (int i = 0; i < size; i++)
+// {
+// array[i] = rnd.Next(min, max + 1);
+// }
+// return array;
+// }
+
+// Печать массива
+
+// void PrintArray(int[] array)
+// {
+// Console.Write("[");
+// for (int i = 0; i < array.Length; i++)
+// {
+// if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+// else Console.Write($"{array[i]}");
+// }
+// Console.WriteLine("]");
+// }
+
+// int GetSumPositiveElem(int[] array)
+// {
+// int sumPositive = default;
+
+// for (int i = 0; i < array.Length; i++)
+// if (array[i] > 0) sumPositive += array[i];
+
+// return sumPositive;
+// }
+// Андрей Булгаков: int GetSumNegativeElem(int[] array)
+// {
+// int sumNegative = default;
+
+// for (int i = 0; i < array.Length; i++)
+// if (array[i] < 0) sumNegative += array[i];
+
+// return sumNegative;
+// }
+
+// int[] arr = CreateArrayRndInt(12, -9, 9);
+// PrintArray(arr);
+// int sumPositive = GetSumPositiveElem(arr);
+// int sumNegative = GetSumNegativeElem(arr);
+// Console.WriteLine($"Сумма положительных элементов = {sumPositive}");
+// Console.WriteLine($"Сумма отрицательных элементов = {sumNegative}");
--- Lvl_33/Program.cs
+++ Lvl_33/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задать массив
+// Задать число
+// Определить входит ли оно туда
+Console.WriteLine("Введите количество элементов массива:");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[num];
+Console.Write("Введите искомое число: ");
+int numFind = Convert.ToInt32(Console.ReadLine());
+
+void FillArray(int[] col)
+{
+    int lenght = col.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        col[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void NumFinder(int[] arr, int find)
+{
+    bool res = false;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == find)
+        {
+            res = true;
+            break;
+        }
+    }
+    if (res == true) Console.WriteLine("Да");
+    else Console.WriteLine("Нет");
+}
+
+// Печать массива
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+FillArray(array);
+PrintArray(array);
+
+NumFinder(array, numFind);
