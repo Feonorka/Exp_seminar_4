15c0a1e403fad578f79958c3818e1a1fa9dcec01
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-10-19 03:00:44 +0300

    Сделаны 35 и 37 задачи

--- Lvl_35/Program.cs
+++ Lvl_35/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+
+Console.WriteLine("Введите количество элементов массива:");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[num];
+
+void FillArray(int[] col, int minN, int maxN)
+{
+    int lenght = col.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        col[index] = new Random().Next(minN, maxN);
+        index++;
+    }
+}
+
+// Печать массива
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+void SearchSumm(int[] array, int min, int max)
+{
+    int resSum = default;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > min && array[i] < max) resSum = resSum + array[i];
+    }
+    Console.WriteLine($"Сумма элементов массива попадающих в интервал от {min} до {max} равна {resSum}");
+}
+
+
+FillArray(array, 1, 124);
+PrintArray(array);
+SearchSumm(array, 10, 99);
--- Lvl_37/Program.cs
+++ Lvl_37/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+Console.WriteLine("Введите количество элементов массива:");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[num];
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+void FillArray(int[] col, int minN, int maxN)
+{
+    int lenght = col.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        col[index] = new Random().Next(minN, maxN);
+        index++;
+    }
+}
+
+int[] Expo(int[] arr)
+{
+    int numRes = arr.Length / 2;
+    if (arr.Length % 2 == 1) numRes++;
+    
+    int[] resArr = new int[numRes];
+
+    for (int i = 0; i < (arr.Length / 2); i++)
+    {
+        resArr[i] = arr[i] * arr[arr.Length - 1 - i];
+    }
+    if (arr.Length % 2 == 1) resArr[numRes - 1] = arr[numRes - 1];
+    return resArr;
+}
+
+FillArray(array, 1, 10);
+
+PrintArray(array);
+int[] emptyArr = Expo(array);
+
+PrintArray(emptyArr);
